81c14868ace14ebec84c5e7381cca522cce53aab
ViníciusCrisol <vinicius.crisol@hotmail.com>
2020-09-02 11:02:31 -0300

    fix

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -19,17 +19,3 @@ export default Appearance.getColorScheme() === 'dark'
       red: '#ec1414',
       purple: '#bf94ff',
     };
-
-//  light: {
-
-//  },
-
-//  dark: {
-//    primary: '#000014',
-//    black: '#efeff1',
-//    gray: '#7f7f8b',
-//    tag: '#323235',
-//    green: '#14b866',
-//    red: '#ec1414',
-//    purple: '#bf94ff',
-//  }
